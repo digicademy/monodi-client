c11e5f700cf3979f0aa213ad969a5a2f9766249f
Thomas Weber <thomas.weber@notengrafik.com>
2013-06-18 08:22:23 +0100

    Suggestion for rudimentary "scroll to make selection visible" feature

--- Resources/public/js/controller/DocumentController.js
+++ Resources/public/js/controller/DocumentController.js
@@ -4,28 +4,43 @@ function DocumentCtrl($scope, $http) {
 			staticStyleElement	: document.getElementById("staticStyle"),
 			dynamicStyleElement	: document.getElementById("dynamicStyle"),
 			musicContainer		: document.getElementById("musicContainer"),
-			xsltUrl				: "/bundles/digitalwertmonodiclient/js/monodi/mei2xhtml.xsl",
-			meiString			: $scope.active.content
+			xsltUrl				: "../public/js/monodi/mei2xhtml.xsl",
+			meiUrl              : "../public/js/monodi/hodieCantandus.mei"//meiString			: $scope.active.content
 		});
 
-		monodi.document.addCallback('deleteAnnotatedElement', function(data) {
-			return confirm('Do you want to delete the ' + data.length + ' Annotations associated with this element?');
-		});
+        $scope.showView('main');
+        
+    });
 
-		var typesrc = monodi.document.ANNOTATION_TYPES || {};
-		types = '<p><select>';
-		for (var type in typesrc) {
-			types += '<option value="' + type + '">' + typesrc[type].label + '</option>';
+    monodi.document = new MonodiDocument({
+		staticStyleElement	: document.getElementById("staticStyle"),
+		dynamicStyleElement	: document.getElementById("dynamicStyle"),
+		musicContainer		: document.getElementById("musicContainer"),
+		xsltUrl				: "../public/js/monodi/mei2xhtml.xsl",
+		meiUrl              : "../public/js/monodi/empty.mei"//meiString			: $scope.active.content
+	});
+
+	monodi.document.addCallback('deleteAnnotatedElement', function(data) {
+		return confirm('Do you want to delete the ' + data.length + ' Annotations associated with this element?');
+	});
+	
+	monodi.document.addCallback("updateView",function(element){
+		var offset = $(element).offset().top - $(window).scrollTop();
+
+		if(offset > window.innerHeight || offset < 0){
+			element.scrollIntoView(offset > 0);
 		}
-		$('#annotationModal').find('input').parent().before(types + '</select></p>');
-
-		$scope.showView('main');
 	});
 
-	$scope.$on('saveDocument', function() {
-		$scope.active.content = monodi.document.getSerializedDocument();
-		$http.put(baseurl + 'api/v1/documents/' + $scope.active.id + '.json?access_token=' + $scope.access_token, angular.toJson($scope.active)).success(function (data) {
-            console.log(data);
-        });
-	});
+	var typesrc = monodi.document.ANNOTATION_TYPES || {};
+	types = '<p><select>';
+	for (var type in typesrc) {
+		types += '<option value="' + type + '">' + typesrc[type].label + '</option>';
+	}
+	$('#annotationModal').find('input').parent().before(types + '</select></p>');
+
+    $scope.showView('main');
+
+    //save: monodi.document.getSerializedDocument()
+
 }
